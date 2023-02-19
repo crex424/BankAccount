791a11c4aa381e68161e39c35e5ac019d636affa
Campbell Rex <campbellrex@outlook.com>
2023-02-19 15:11:38 -0800

    Add project files.

---
+++ BankAccount/Account.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+    /// <summary>
+    ///  Represents a single customers bank account
+    /// </summary>
+    public class Account
+    {
+        /// <summary>
+        /// Creates an account with specific owner and a balance of 0
+        /// </summary>
+        /// <param name="accountOwner">The customer full name that owns account</param>
+        public Account(string accountOwner)
+        {
+            Owner = accountOwner;
+        }
+
+        /// <summary>
+        /// The account holders full name (first last)
+        /// </summary>
+        public string Owner { get; set; }
+        /// <summary>
+        /// The amount of money in the account
+        /// </summary>
+        public double Balance { get; private set; }
+        /// <summary>
+        /// Add a specified amount of money to account
+        /// </summary>
+        /// <param name="amount">The positive amount to deposit</param>
+        public void Deposit(double amount)
+        {
+            throw new NotImplementedException();
+        }
+        /// <summary>
+        /// Withdraws an amount of money from the balance
+        /// </summary>
+        /// <param name="amount">  The positive amount of money to be removed from the balance</param>
+        public void Withdraw(double amount)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
