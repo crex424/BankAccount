101fd0bae51e873d3d195b9a94c93cd98f149c98
Campbell Rex <campbellrex@outlook.com>
2023-02-20 18:28:37 -0800

    Implemented withdraw tests

--- BankAccount/Account.cs
+++ BankAccount/Account.cs
@@ -52,6 +52,14 @@ namespace BankAccount
         /// <returns>Returns updated balance after withdrawal</returns>
         public double Withdraw(double amount)
         {
+            if (amount > Balance)
+            {
+                throw new ArgumentException($"{nameof(amount)} cannot be greater than {nameof(Balance)}");
+            }
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException($"{nameof(amount)} must be greater than 0");
+            }
             Balance -= amount;
             return Balance;
         }
--- BankAccountTests/AccountTests.cs
+++ BankAccountTests/AccountTests.cs
@@ -77,24 +77,36 @@ namespace BankAccount.Tests
         }
 
         [TestMethod]
-        public void Withdraw_PositiveAmount_ReturnsUpdatedBalance()
+        [DataRow(100, 50)]
+        [DataRow(100, 0.99)]
+        public void Withdraw_PositiveAmount_ReturnsUpdatedBalance(double initialDeposit, double withdrawalAmount)
         {
-            Assert.Fail();
+            // Arrange
+            double expectedBalance = initialDeposit - withdrawalAmount;
+            acc.Deposit(initialDeposit);
+            // Act
+            double returnedBalance = acc.Withdraw(withdrawalAmount);
+
+            // Assert
+            Assert.AreEqual(expectedBalance, returnedBalance);
         }
 
         [TestMethod]
         [DataRow(0)]
         [DataRow(-.01)]
         [DataRow(-1000)]
-        public void Withdraw_ZeroOrLess_ThrowsArgumentOutOfRangeException()
+        public void Withdraw_ZeroOrLess_ThrowsArgumentOutOfRangeException(double withdrawAmount)
         {
-            Assert.Fail();
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => acc.Withdraw(withdrawAmount));
+
         }
 
         [TestMethod]
         public void Withdraw_MoreThanAvailableBalance_ThrowsArgumentException()
         {
-            Assert.Fail();
+            double withdrawalAmount = 1000;
+
+            Assert.ThrowsException<ArgumentException>(() => acc.Withdraw(withdrawalAmount));
         }
     }
 }
